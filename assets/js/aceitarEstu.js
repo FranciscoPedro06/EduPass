20c0dd1f137895b5af0c87086833aa37eeff60bc
Matheus Santana <mssantana@ecomp.uefs.br>
2025-11-10 20:22:43 -0300

    update

--- assets/js/aceitarEstu.js
+++ assets/js/aceitarEstu.js
@@ -1,4 +1,4 @@
-import { db } from "/assets/js/firebase-config.js";
+import { db } from "./firebase-config.js";
 import { 
 collection, getDocs, getDoc, addDoc, deleteDoc, doc 
 } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
